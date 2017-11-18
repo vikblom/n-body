b02854d480ad6ce856bccc0e981ed17b45744dca
Viktor Blomqvist <veblomqvist@gmail.com>
2017-11-18 16:46:39 +0100

    Visalization is now in 3d (but without blitting).

--- animation/visualize.py
+++ animation/visualize.py
@@ -1,47 +1,55 @@
 #!/usr/bin/env python3
 
-from sys import argv
+
+from sys import argv, stdin
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import rcParams
 from matplotlib.animation import FuncAnimation, writers
 
+#a = np.fromstring(stdin.readline(), sep=" ")
+
 rcParams['toolbar'] = 'None'
 
-fig, ax = plt.subplots()
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-fig.set_facecolor('black')
+#fig.set_facecolor('black')
 
 ax.set_xlim(-5,5)
 ax.set_ylim(-5,5)
+ax.set_zlim(-5,5)
 
-points = ax.scatter([],[], s=1.0, c='w')
+points = ax.scatter([],[],[], s=1.0, c='b')
 
-dt = 10 / 1000
 N = 100
 
 mass = np.random.gamma(2.0, 1.0, N)
 
 x = np.random.normal(0, .1, N)
 y = np.random.normal(0, .1, N)
-vx = np.random.normal(0, dt*1, N)
-vy = np.random.normal(0, dt*1, N)
+z = np.random.normal(0, .1, N)
+vx = np.random.normal(0, .1, N)
+vy = np.random.normal(0, .1, N)
+vz = np.random.normal(0, .1, N)
 
 points.set_sizes(mass)
 
 def update(data):
-    global x, y
+    global x, y, z
     x += vx
     y += vy
-    points.set_offsets(np.c_[x,y])
+    z += vz
+    points._offsets3d = (x,y,z)
     return points,
 
 
 anim = FuncAnimation(fig, update,
                      frames=500,
-                     interval=dt,
+                     interval=25,
                      repeat=False,
-                     blit=True)
+                     blit=False)
 
 plt.axis('off')
 plt.tight_layout()
